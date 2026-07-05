59e63873b5cd5dfde83846c48421404312eee0ee
Joao Paulo <joao.almeida_msbrasil@outlook.com>
2026-07-05 19:07:42 -0300

    feat: hide particle canvas on admin routes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,16 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, inject } from '@angular/core';
+import { RouterOutlet, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { ToastContainerComponent } from './components/toast-container/toast-container.component';
 import { ParticleCanvasComponent } from './components/particle-canvas/particle-canvas.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ToastContainerComponent, ParticleCanvasComponent],
+  imports: [RouterOutlet, ToastContainerComponent, ParticleCanvasComponent, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('agenda-app');
+  public router = inject(Router);
 }
